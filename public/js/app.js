23f700ea8e5d9c80655ad5b1d4cf53b27d2827d4
Melanie Malone <melaniemalone@Melanie-Malones-MacBook-Pro.local>
2017-06-08 18:17:42 -0500

    implemented the add to do method so that people can add todos to the list. cleaned up the mock.

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,8 @@
 /* jshint esversion:6*/
+
 const TodoApp = {
   rootElement: '#app',
-  todos: [{task: 'get some milk', isComplete: false}],
+  todos: [],
   start: function(){
     this.cacheDOM();
     this.bindEvents();
@@ -14,13 +15,32 @@ const TodoApp = {
     this.todoList = this.root.querySelector('.todo-list');
   },
   bindEvents: function(){
-    this.addButton.addEventListener('click', () => console.log('add'));
+    this.addButton.addEventListener('click', () => this.addTodo());
+  },
+  addTodo: function(){
+    // first: grab the task input value
+    const taskValue = this.taskInput.value;
+    // first-part2: validate that taskValue is actually "something"
+      if(!taskValue){
+        return;
+      }
+    // second: build to do object with that value
+    const todo = {
+      task: taskValue,
+      isComplete: false,
+    };
+    // third: add that todo to the todos array
+    this.todos.push(todo);
+    // fourth: rerender
+    this.render();
+    // fifth: clear the input
+    this.taskInput.value = '';
   },
   render: function(){
     const lis = this.todos
                   .map(todo => `<li>${todo.task}</li>`)
-                  .join();
-    this.todoList.innerHTML = lis;
+                  .join('');
+    this.todoList.innerHTML = lis; //be careful, can lead to security vulnerability
   }
 };
 TodoApp.start();
